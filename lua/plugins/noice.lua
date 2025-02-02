13bb176572019c1b177d071c859b03e088228c72
Tucker Fritz <tuckerpfritz@gmail.com>
2025-02-01 21:16:04 -0600

    Add noice plugin

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,13 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+	opts = {
+		cmdline = {
+			enabled = true,
+		},
+	},
+}
