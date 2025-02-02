2038dde7a018030d95e16eeec2a41b94a30d013a
Tucker Fritz <tuckerpfritz@gmail.com>
2025-02-01 20:16:47 -0600

    Just use keys in nvim-tree plugin

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -14,23 +14,13 @@ return {
 			},
 		},
 	},
-	config = function(_, opts)
-		require("nvim-tree").setup(opts)
-		-- Keybindings to navigate between nvim-tree and the buffer
-		vim.api.nvim_set_keymap(
-			"n",
-			"<C-h>",
-			'<cmd>lua require("nvim-tree.api").tree.focus()<CR>',
-			{ noremap = true, silent = true }
-		)
-		vim.api.nvim_set_keymap(
-			"n",
-			"<C-l>",
-			'<cmd>lua if vim.bo.filetype == "NvimTree" then vim.cmd("wincmd l") end<CR>',
-			{ noremap = true, silent = true }
-		)
-	end,
 	keys = {
 		{ "<leader>e", ":NvimTreeToggle<CR>", desc = "Toggle NvimTree" },
+		{ "<C-h>", "<cmd>lua require('nvim-tree.api').tree.focus()<CR>", desc = "Focus NvimTree" },
+		{
+			"<C-l>",
+			"<cmd> lua if vim.bo.filetype == 'NvimTree' then vim.cmd('wincmd l') end<CR>",
+			desc = "Unfocus NvimTree",
+		},
 	},
 }
