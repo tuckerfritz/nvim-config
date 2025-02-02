e3c46dd2c9c7d3f902dd525780ebe0b932dd763d
Tucker Fritz <tuckerpfritz@gmail.com>
2025-02-01 20:21:39 -0600

    run formatter

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,12 +1,12 @@
 return {
-  "catppuccin/nvim",
-  lazy = false,
-  name = "catppuccin",
-  priority = 1000,
-  opts = {
-    flavour = "mocha"
-  },
-  config = function()
-    vim.cmd.colorscheme("catppuccin")
-  end
+	"catppuccin/nvim",
+	lazy = false,
+	name = "catppuccin",
+	priority = 1000,
+	opts = {
+		flavour = "mocha",
+	},
+	config = function()
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
